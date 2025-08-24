fdb69ca5ebd1d27b00c4f18f45563a1556d3c6c6
Andras Fejes <fejes.andras@dev-labs.com>
2025-08-24 17:14:40 +0200

    Added description annotations to the Address and Person

--- src/main/java/io/crunch/ai/common/Address.java
+++ src/main/java/io/crunch/ai/common/Address.java
@@ -1,4 +1,12 @@
 package io.crunch.ai.common;
 
-public record Address(String country, String city, String zipCode, String street, String houseNumber) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.langchain4j.model.output.structured.Description;
+
+public record Address(
+        @Description("The user's address country") @JsonProperty(required = true) String country,
+        @Description("The user's address city") @JsonProperty(required = true) String city,
+        @Description("The user's address zip code") @JsonProperty(required = true) String zipCode,
+        @Description("The user's address street") @JsonProperty(required = true) String street,
+        @Description("The user's address house number") @JsonProperty(required = true) String houseNumber) {
 }
--- src/main/java/io/crunch/ai/common/Person.java
+++ src/main/java/io/crunch/ai/common/Person.java
@@ -1,4 +1,10 @@
 package io.crunch.ai.common;
 
-public record Person(String firstName, String lastName, String birthDate) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.langchain4j.model.output.structured.Description;
+
+public record Person(
+        @Description("The user's first name") @JsonProperty(required = true) String firstName,
+        @Description("The user's last name") @JsonProperty(required = true) String lastName,
+        @Description("The user's birth date") @JsonProperty(required = true) String birthDate) {
 }
